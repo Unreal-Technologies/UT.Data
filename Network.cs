fa5f0e4b614f5d1764c87beda1a60a71291864cd
Peter Overeijnder <peterovereijnder@hotmail.com>
2022-08-30 09:08:32 +0200

    -

---
+++ Network.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace UT.Data
+{
+    public class Network
+    {
+        #region Public Methods
+        public static IPAddress[] LocalIPv4(NetworkInterfaceType nit)
+        {
+            List<IPAddress> buffer = new();
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType == nit && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            buffer.Add(ip.Address);
+                        }
+                    }
+                }
+            }
+            return buffer.ToArray();
+        }
+        #endregion //Public Methods
+    }
+}
