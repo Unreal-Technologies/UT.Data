8965f5e82818d428e47c63f6c87e76afcf660484
Peter Overeijnder <peterovereijnder@hotmail.com>
2022-10-08 00:06:59 +0200

    -

---
+++ Hashing.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace UT.Data
+{
+    public class Hashing
+    {
+        public static Guid Guid(string input)
+        {
+            using MD5 md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(Encoding.Default.GetBytes(input));
+            return new Guid(hash);
+        }
+    }
+}
