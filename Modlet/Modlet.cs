d3e03573a5c1e499e1c5b57b88bcaf3081525e9f
Peter Overeijnder <peterovereijnder@hotmail.com>
2022-10-11 07:47:59 +0200

    -

---
+++ Modlet/Modlet.cs
@@ -0,0 +1,81 @@
+﻿using System.Reflection;
+
+namespace UT.Data.Modlet
+{
+    public class Modlet
+    {
+        public static IModlet[] Load(SequentialExecution? se)
+        {
+            List<IModlet> buffer = new();
+            Assembly[] assemblies = Modlet.GetAssemblies();
+            
+            foreach(Assembly assembly in assemblies)
+            {
+                foreach(Type type in assembly.GetExportedTypes())
+                {
+                    if(!type.IsInterface)
+                    {
+                        Type? modlet = type.GetInterfaces().Where(x => x == typeof(IModlet)).FirstOrDefault();
+                        if(modlet != null)
+                        {
+                            IModlet? instance = (IModlet?)Activator.CreateInstance(type);
+                            if (instance != null)
+                            {
+                                if (se != null)
+                                {
+                                    instance.OnSequentialExecutionConfiguration(se);
+                                }
+                                buffer.Add(instance);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return buffer.ToArray();
+        }
+
+        private static Assembly[] GetAssemblies()
+        {
+            List<Assembly> assemblies = new List<Assembly>();
+            assemblies.Add(Assembly.GetExecutingAssembly());
+
+            Assembly? entryAssem = Assembly.GetEntryAssembly();
+            if (entryAssem != null)
+            {
+                assemblies.Add(entryAssem);
+            }
+
+            Assembly? modletAssem = Assembly.GetAssembly(typeof(IModlet));
+            if (modletAssem == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            FileInfo file = new(modletAssem.Location);
+            DirectoryInfo? root = file.Directory;
+            if (root == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            foreach (FileInfo fi in root.EnumerateFiles("*.dll"))
+            {
+                if (fi.FullName == file.FullName)
+                {
+                    continue;
+                }
+                Assembly? match = assemblies.Where(x => x.Location == fi.FullName).FirstOrDefault();
+                if (match != null)
+                {
+                    continue;
+                }
+
+                Assembly assem = Assembly.LoadFile(fi.FullName);
+                assemblies.Add(assem);
+            }
+
+            return assemblies.ToArray();
+        }
+    }
+}
