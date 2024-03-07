ca8243a0a91e0c9746a34f1bb818b8912e5d328e
Peter Overeijnder <peterovereijnder@hotmail.com>
2024-03-07 22:46:02 +0100

    -

--- Modlet/IModlet.cs
+++ Modlet/IModlet.cs
@@ -5,7 +5,7 @@ namespace UT.Data.Modlet
     public interface IModlet
     {
         public void OnSequentialExecutionConfiguration(SequentialExecution se);
-        public void OnClientConfiguration(ModletClient client, Form? form);
+        public void OnClientConfiguration(Form? form);
         public void OnGlobalServerAction(byte[]? stream);
         public byte[]? OnLocalServerAction(byte[]? stream);
         public void OnServerConfiguration(DbContext? context, ref Dictionary<string, object?> configuration);
